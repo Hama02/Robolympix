2b4d1e00f67d53cf1f1d484aafa02f6d8e65a5d8
Mohamed <mohamedbenjdira5@gmail.com>
2024-03-18 02:47:42 +0100

    fix assets directory

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,27 +4,43 @@ import { Heading, Image, Text, VStack } from "@chakra-ui/react";
 const About = () => {
   return (
     <FullScreenSection
-      height={['100%', '125vh']}
+      height={["100%", "125vh"]}
       id="intro-section"
       justifyContent="center"
       alignItems="center"
       backgroundColor="#ffffff"
-     // backgroundColor="rgba(229, 231, 235, 0.8)" // Set the background color to transparent with 0.8 alpha
+      // backgroundColor="rgba(229, 231, 235, 0.8)" // Set the background color to transparent with 0.8 alpha
     >
       <VStack zIndex={1} spacing={10}>
-        <Heading marginTop={["5%", "25%"]} color={'var(--title)'} fontSize={["xl", "5xl"]}>
+        <Heading
+          marginTop={["5%", "25%"]}
+          color={"var(--title)"}
+          fontSize={["xl", "5xl"]}
+        >
           A propos
         </Heading>
         <Text
           id="landing-text"
           margin="5% 4% 3% 4%"
-          style={{ fontFamily: 'cursive', whiteSpace: 'pre-wrap' }}
+          style={{ fontFamily: "cursive", whiteSpace: "pre-wrap" }}
           fontSize={["sm", "xl"]}
         >
-          Robolympix est l’évènement scientifique incontournable dans sa deuxième édition. C’est l’une des plus grandes compétitions annuelles de robotique en Tunisie. Lancé en 2021 par L’AEROBOTIX INSAT pour stimuler l’innovation, cet évènement comporte dans cette 2eme version de nombreux enjeux scientifiques et technologiques. La Robolympix V 2.0 rassemble les meilleures équipes de robotique en Tunisie et le monde entier. <br />La première édition de Robolympix était le 19/12/2021. Vous pouvez revivre cette édition avec notre galerie !
+          Robolympix est l’évènement scientifique incontournable dans sa
+          deuxième édition. C’est l’une des plus grandes compétitions annuelles
+          de robotique en Tunisie. Lancé en 2021 par L’AEROBOTIX INSAT pour
+          stimuler l’innovation, cet évènement comporte dans cette 2eme version
+          de nombreux enjeux scientifiques et technologiques. La Robolympix V
+          2.0 rassemble les meilleures équipes de robotique en Tunisie et le
+          monde entier. <br />
+          La première édition de Robolympix était le 19/12/2021. Vous pouvez
+          revivre cette édition avec notre galerie !
         </Text>
 
-        <Image marginBottom={['-25%', "10%"]} height={["100px", "200px"]} src="\public\cool-robot.png" />
+        <Image
+          marginBottom={["-25%", "10%"]}
+          height={["100px", "200px"]}
+          src="\cool-robot.png"
+        />
       </VStack>
     </FullScreenSection>
   );
--- src/components/Challenges.jsx
+++ src/components/Challenges.jsx
@@ -19,14 +19,12 @@ const Challenges = () => {
       id="challenges-section"
       justifyContent="center"
       alignItems="center"
-
-
     >
-      <Heading color={'var(--title)'} as="h1" size="2xl" paddingBottom="5%">
+      <Heading color={"var(--title)"} as="h1" size="2xl" paddingBottom="5%">
         Challenges
       </Heading>
       <HStack spacing={8} align="center" maxW="1200px">
-      <Card
+        <Card
           direction="column"
           overflow="hidden"
           borderRadius="10px"
@@ -37,13 +35,15 @@ const Challenges = () => {
           <Image
             zIndex={1}
             borderRadius="10px 10px 0 0"
-            src="\public\suiveur.png"
+            src="\suiveur.png"
             alt="robolympix"
             height={["200px", "300px"]}
             width="100%"
             objectFit="cover"
           />
-          <HStack p={4} spacing={4} align="start"> {/* Change VStack to HStack here */}
+          <HStack p={4} spacing={4} align="start">
+            {" "}
+            {/* Change VStack to HStack here */}
             <Heading as="h2" size="lg" color="#1E293B">
               RACE IT
             </Heading>
@@ -51,9 +51,13 @@ const Challenges = () => {
               {/* Add your description here */}
             </Text>
           </HStack>
-          <CardFooter style={{ justifyContent: "center", alignItems: "center" }}>
+          <CardFooter
+            style={{ justifyContent: "center", alignItems: "center" }}
+          >
             <a href="https://drive.google.com/file/d/1gECYVhySzYKvsXEDzyvOuwohYR-hYXT_/view?fbclid=IwAR2QF5Cyb56odcIYli-2qmivnioYsJnLqntQavGKIMpwM72ShynIlAoUWrg">
-              <Button colorScheme="blackAlpha" variant="solid">View More</Button>
+              <Button colorScheme="blackAlpha" variant="solid">
+                View More
+              </Button>
             </a>
           </CardFooter>
         </Card>
@@ -74,7 +78,9 @@ const Challenges = () => {
             width="100%"
             objectFit="cover"
           />
-          <HStack p={4} spacing={4} align="start"> {/* Change VStack to HStack here */}
+          <HStack p={4} spacing={4} align="start">
+            {" "}
+            {/* Change VStack to HStack here */}
             <Heading as="h2" size="lg" color="#1E293B">
               DERBY
             </Heading>
@@ -82,9 +88,13 @@ const Challenges = () => {
               {/* Add your description here */}
             </Text>
           </HStack>
-          <CardFooter style={{ justifyContent: "center", alignItems: "center" }}>
+          <CardFooter
+            style={{ justifyContent: "center", alignItems: "center" }}
+          >
             <a href="https://drive.google.com/file/d/1yjuORB5hbp-ris22EYqFYLIc1xbh05Lh/view?fbclid=IwAR1IgLdV_fCkf9SLnD2TR6tALgAqD575Ib5I3QqnrgQd4DY0F3wQS8vklsY">
-              <Button colorScheme="blackAlpha" variant="solid">View More</Button>
+              <Button colorScheme="blackAlpha" variant="solid">
+                View More
+              </Button>
             </a>
           </CardFooter>
         </Card>
@@ -100,23 +110,29 @@ const Challenges = () => {
           <Image
             zIndex={1}
             borderRadius="10px 10px 0 0"
-            src="/public/NAVIGATOR.jpg"
+            src="/NAVIGATOR.jpg"
             alt="robolympix"
             height={["200px", "300px"]}
             width="100%"
             objectFit="cover"
           />
-          <HStack p={4} spacing={4} align="start"> {/* Change VStack to HStack here */}
+          <HStack p={4} spacing={4} align="start">
+            {" "}
+            {/* Change VStack to HStack here */}
             <Heading as="h2" size="lg" color="#1E293B">
-            NAVIGATOR
+              NAVIGATOR
             </Heading>
             <Text fontSize={["sm", "md"]} color="#4B5563">
               {/* Add your description here */}
             </Text>
           </HStack>
-          <CardFooter style={{ justifyContent: "center", alignItems: "center" }}>
+          <CardFooter
+            style={{ justifyContent: "center", alignItems: "center" }}
+          >
             <a href="https://drive.google.com/file/d/1EEH9i54ei0rInYGIdFdZxAeoPiXw9jx1/view?usp=sharing&fbclid=IwAR1GzvYz6LXpjLi4t2Uird7PYk7KTvqzU2b4sJARzJYx8dscTZkKHA_kL_s">
-              <Button colorScheme="blackAlpha" variant="solid">View More</Button>
+              <Button colorScheme="blackAlpha" variant="solid">
+                View More
+              </Button>
             </a>
           </CardFooter>
         </Card>
@@ -128,32 +144,36 @@ const Challenges = () => {
           backgroundColor="#F3F4F6"
           boxShadow="0 4px 8px rgba(0, 0, 0, 0.1)"
         >
-          
           <Image
             zIndex={1}
             borderRadius="10px 10px 0 0"
-            src="\public\THE ADVENTURE.jpg"
+            src="\THE ADVENTURE.jpg"
             alt="robolympix"
             height={["200px", "300px"]}
             width="100%"
             objectFit="cover"
           />
-          <HStack p={4} spacing={4} align="start"> {/* Change VStack to HStack here */}
+          <HStack p={4} spacing={4} align="start">
+            {" "}
+            {/* Change VStack to HStack here */}
             <Heading as="h2" size="sm" color="#1E293B">
-              THE <br/>
+              THE <br />
               ADVENTURE
             </Heading>
             <Text fontSize={["sm", "md"]} color="#4B5563">
               {/* Add your description here */}
             </Text>
           </HStack>
-          <CardFooter style={{ justifyContent: "center", alignItems: "center" }}>
+          <CardFooter
+            style={{ justifyContent: "center", alignItems: "center" }}
+          >
             <a href="https://drive.google.com/file/d/1WwRb6pvD3I4zS_Fg5rcRXEzCQwulhztG/view?fbclid=IwAR3cZJPGV38lytg3tGzG3HvLv91xT9XPxAnPWMRu1Yktwc-vyEXgDyynn9Q">
-              <Button colorScheme="blackAlpha" variant="solid">View More</Button>
+              <Button colorScheme="blackAlpha" variant="solid">
+                View More
+              </Button>
             </a>
           </CardFooter>
         </Card>
-        
       </HStack>
     </FullScreenSection>
   );
